462b306536d1e75a4ac30384eaa39eee6589dc4c
Bojan Branovacki <bojan.branovacki@gmail.com>
2025-10-01 14:07:24 +0200

    feat: squashed commit of solution hold to save button

--- apps/rxjs/49-hold-to-save-button/src/app/app.component.ts
+++ apps/rxjs/49-hold-to-save-button/src/app/app.component.ts
@@ -1,25 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DelayedClickDirective } from './delayedClick';
 
 @Component({
-  imports: [],
+  imports: [DelayedClickDirective],
   selector: 'app-root',
   template: `
     <main class="flex h-screen items-center justify-center">
       <div
         class="flex w-full max-w-screen-sm flex-col items-center gap-y-8 p-4">
         <button
+          appDelayedClick
+          [holdTime]="1000"
+          (progressChange)="handleProgressUpdate($event)"
+          (clickFinalized)="onSend()"
           class="rounded bg-indigo-600 px-4 py-2 font-bold text-white transition-colors ease-in-out hover:bg-indigo-700">
           Hold me
         </button>
 
-        <progress [value]="20" [max]="100"></progress>
+        <progress [value]="progress()" [max]="100"></progress>
       </div>
     </main>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  progress = signal(0);
+
+  handleProgressUpdate(value: number) {
+    this.progress.set(value);
+  }
+
   onSend() {
-    console.log('Save it!');
+    console.log('Action triggered!');
+    alert('Action triggered!');
   }
 }
