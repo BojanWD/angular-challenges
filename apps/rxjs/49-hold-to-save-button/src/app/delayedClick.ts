916f613f65ce324aba94c79952f74807439fb0fb
Bojan Branovacki <bojan.branovacki@gmail.com>
2025-10-01 13:47:19 +0200

    feat: create holdable directive

---
+++ apps/rxjs/49-hold-to-save-button/src/app/delayedClick.ts
@@ -0,0 +1,66 @@
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  inject,
+  Input,
+  Output,
+  signal,
+} from '@angular/core';
+import {
+  finalize,
+  fromEvent,
+  interval,
+  map,
+  merge,
+  switchMap,
+  takeUntil,
+  takeWhile,
+} from 'rxjs';
+
+@Directive({
+  selector: '[appDelayedClick]',
+})
+export class DelayedClickDirective {
+  @Input() holdTime = 2000;
+  @Output() progressChange = new EventEmitter<number>();
+  @Output() clickFinalized = new EventEmitter<void>();
+
+  progress = signal(0);
+
+  private el = inject(ElementRef);
+  constructor() {
+    const el = this.el.nativeElement;
+    const mousedown$ = fromEvent<MouseEvent>(el, 'mousedown');
+    const mouseup$ = fromEvent<MouseEvent>(el, 'mouseup');
+    const leave$ = fromEvent<MouseEvent>(el, 'mouseleave');
+    const cancel$ = merge(mouseup$, leave$);
+
+    mousedown$
+      .pipe(
+        switchMap(() => {
+          const start = Date.now();
+          return interval(10).pipe(
+            map(() => Date.now() - start),
+            map((elapsed) => Math.min((elapsed / this.holdTime) * 100, 100)),
+            takeWhile((p) => p < 100, true),
+            takeUntil(cancel$),
+            finalize(() => {
+              if (this.progress() >= 100) {
+                this.clickFinalized.emit(); // fire only on completion
+              }
+              this.changeProgress(0);
+            }),
+          );
+        }),
+      )
+      .subscribe((p) => {
+        this.changeProgress(p);
+      });
+  }
+
+  private changeProgress(v: number) {
+    this.progress.set(v);
+    this.progressChange.emit(v);
+  }
+}
