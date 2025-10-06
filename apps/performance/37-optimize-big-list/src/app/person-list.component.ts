587e1c3cc9c2974e3d5cb44d26e8cb1761250ea7
Bojan Branovacki <bojan.branovacki@gmail.com>
2025-10-06 18:16:36 +0200

    fix: optimize display of list by implementing virtualization

--- apps/performance/37-optimize-big-list/src/app/person-list.component.ts
+++ apps/performance/37-optimize-big-list/src/app/person-list.component.ts
@@ -1,3 +1,4 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Person } from './person.model';
 
@@ -5,21 +6,30 @@ import { Person } from './person.model';
   selector: 'app-person-list',
   template: `
     <div class="relative h-[300px] overflow-hidden">
-      <div class="absolute inset-0 overflow-scroll">
-        @for (person of persons(); track person.email) {
-          <div class="flex h-9 items-center justify-between border-b">
-            <h3>{{ person.name }}</h3>
-            <p>{{ person.email }}</p>
-          </div>
-        }
-      </div>
+      <!-- <div > -->
+      <cdk-virtual-scroll-viewport
+        itemSize="36"
+        class="absolute inset-0 overflow-scroll">
+        <div
+          *cdkVirtualFor="let person of persons()"
+          class="flex h-9 items-center justify-between border-b">
+          <h3>{{ person.name }}</h3>
+          <p>{{ person.email }}</p>
+        </div>
+      </cdk-virtual-scroll-viewport>
     </div>
   `,
   host: {
     class: 'w-full flex flex-col',
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ScrollingModule],
 })
 export class PersonListComponent {
   persons = input<Person[]>();
+
+  trackbyemail(index: number, item: Person) {
+    console.log(index, item);
+    return item.email;
+  }
 }
