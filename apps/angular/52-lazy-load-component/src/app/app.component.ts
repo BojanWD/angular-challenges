1405772edc0eaa08c17fbfdab9342647ed89dfbc
Bojan Branovacki <bojan.branovacki@gmail.com>
2025-10-16 14:09:24 +0200

    feat: introduce defer to lazily load component

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -1,23 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { PlaceholderComponent } from './placeholder.component';
+import { TopComponent } from './top.component';
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="h-screen bg-gray-500">
-      @if (topLoaded()) {
+      @defer (on interaction(greeting)) {
         <app-top />
-      } @else {
-        <app-placeholder />
+      } @placeholder {
+        <div class="contents">
+          <app-placeholder />
+        </div>
         <button
-          class="rounded-sm border border-blue-500 bg-blue-300 p-2"
-          (click)="topLoaded.set(true)">
+          #greeting
+          class="rounded-sm border border-blue-500 bg-blue-300 p-2">
           Load Top
         </button>
       }
     </div>
   `,
-  standalone: false,
+  standalone: true,
+  imports: [TopComponent, PlaceholderComponent],
 })
-export class AppComponent {
-  topLoaded = signal(false);
-}
+export class AppComponent {}
